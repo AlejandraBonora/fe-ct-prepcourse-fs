0964c6ed24f9938cc324fd1d285812228bb4a1c8
Ale Bonora <ale.bonora@gmail.com>
2023-08-26 21:13:11 -0300

    Modulo 4 homework done

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -46,14 +46,14 @@ function saludo(idioma) {
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
    if (idioma === "aleman"){
-      return ("Guten Tag!");
+      return "Guten Tag!";
 
    }else if(idioma === "mandarin"){
-      return ("Ni Hao!");
+      return "Ni Hao!";
    }else if(idioma === "ingles"){
-      return ("Hello!");
+      return "Hello!";
    }else{
-      return ("Hola!");
+      return "Hola!";
    }
 }
 
@@ -109,7 +109,11 @@ function esEntero(num) {
    // Ejemplo: (-10) ---> true
    // De lo contrario, retorna false.
    // Tu código:
-
+   if (Number.isInteger(num)){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 function fizzBuzz(num) {
@@ -138,6 +142,17 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+   if(num1 > num2 && num1 > num3 && num1 > 0){
+      return ("Numero 1 es mayor y positivo");
+   }else if (num1 < 0 || num2 < 0 || num3 < 0){
+      return ("Hay negativos");
+   }else if (num3 > num1 && num3 > num2){
+      return (num3 + 1);
+   } else if (num1 === 0 && num2 === 0 && num3 === 0 ){
+      return("Error")
+   }else{
+      return false
+   }
 }
 
 function esPrimo(num) {
@@ -147,18 +162,41 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
-}
+   if (num  <= 1){
+      return false;
+   }  
+   for (let i = 2; i < num; i++) {
+         if (num % i === 0){
+            return false;
+       }
+   }
+   return true;  
+   }
+  
+
+
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+   if(valor === true){
+      return "Soy verdadero"
+   }else{
+      return "Soy falso"
+
+   }
 }
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+   if(num >= 100 && num <= 999){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 function doWhile(num) {
@@ -166,6 +204,12 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+   let count = 0;
+   do{
+      num += 5;
+      count++;
+   } while (count < 8);
+   return num;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
